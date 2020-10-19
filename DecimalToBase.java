64577d0b43a7189fa620431e3fecba3d5bb9e7e9
Taran Sidhu <46233992+TAR2D@users.noreply.github.com>
2020-10-19 14:09:24 -0500

    Add files via upload

---
+++ DecimalToBase.java
@@ -0,0 +1,19 @@
+//Help you with converting decimal to base. Not for practicing. Just to save some time.
+public class DecimalToBase{
+	public static void main(String[] args){
+		int base = 3;
+		int decimalNum = 437;
+		decToBase(decimalNum,base);
+	}
+
+	public static void decToBase(int decNumb, int base){
+		int result = out;
+		int remainder = decNumb % base;
+		int temp = decNumb/base;
+		if(decNumb != 0){
+			System.out.println(decNumb + " = "+ base + "*" + temp+ " + "+remainder);
+			decNumb = base*temp;
+			decToBase(temp,base);
+		}
+	}
+}
